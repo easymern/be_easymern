b6318e045434f06c179a730f466af37c654c84b6
andrew <andrewtclarkson@gmail.com>
2022-05-11 09:00:42 -0400

    change DEFAULT_PORT to 3001 to match API_URL in fe_easymern code

--- server.js
+++ server.js
@@ -7,21 +7,21 @@ const path = require("path");
 const fs = require("fs");
 const Role = db.Role;
 
-const DEFAULT_PORT = 8080
-const CORS_PORT = 3000
+const DEFAULT_PORT = 3001;
+const CORS_PORT = 3000;
 
 const { initialRole } = require("./app/models/initialise");
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 const app = express();
 const PORT = process.env.PORT || DEFAULT_PORT;
 
 const corsOptions = {
-  origin: `http://localhost:${CORS_PORT}`
-}
+  origin: `http://localhost:${CORS_PORT}`,
+};
 
 // app.use((req, res, next) => {
 //   res.setHeader('Access-Control-Allow-Origin', 'true');
@@ -40,18 +40,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Connect to mongoDB
-const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/${process.env.DB_DATABASE}?retryWrites=true&w=majority`
+const dbURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_CLUSTER}/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
 
 db.mongoose
   .connect(dbURI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Successfully connected to MongoDB.");
     // initial();
   })
-  .catch(err => {
+  .catch((err) => {
     console.error("Connection error", err);
     process.exit();
   });
@@ -65,27 +65,27 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // image storage
-app.use('/uploads/images', express.static(path.join('uploads', 'images')));
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
   next();
 });
 
 app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route.', 404);
+  const error = new HttpError("Could not find this route.", 404);
   throw error;
 });
 
 app.use((error, req, res, next) => {
   if (req.file) {
-    fs.unlink(req.file.path, err => {
+    fs.unlink(req.file.path, (err) => {
       console.log(err);
     });
   }
@@ -93,14 +93,15 @@ app.use((error, req, res, next) => {
     return next(error);
   }
   res.status(error.code || 500);
-  res.json({ message: error.message || 'An unknown error occurred!' });
+  res.json({ message: error.message || "An unknown error occurred!" });
 });
 
 // set port, listen for requests
 app.listen(PORT, () => {
-  console.log(`==> 🌎 Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`);
+  console.log(
+    `==> 🌎 Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`
+  );
 });
-
 
 // Load initial data
 initialRole();
